7c952a361bea4146c8fd4140242b9fc5ff2b1ef1
DESKTOP-T0HLH7K\os <vanchanhnguyen95@gmail.com>
2022-11-20 14:57:52 +0700

    7: Code đổi tên Map  8: Refresh TOC

--- Study01/ArcMapAddin1/ArcMapAddin1/ButtonDemo.cs
+++ Study01/ArcMapAddin1/ArcMapAddin1/ButtonDemo.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -17,6 +19,15 @@ namespace ArcMapAddin1
             //  TODO: Sample code showing how to access button host
             //
             ArcMap.Application.CurrentTool = null;
+
+            // 7: Code đổi tên Map
+            IMxDocument mxdoc = ArcMap.Document as IMxDocument;
+            IMap map = mxdoc.FocusMap;
+            map.Name = "My New Map 2";
+
+            // 8: Refresh TOC
+            IContentsView contentsView = mxdoc.ContentsView[0];
+            contentsView.Refresh(null);
         }
         protected override void OnUpdate()
         {
