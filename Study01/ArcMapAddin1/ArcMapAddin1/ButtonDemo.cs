51c3bc39fc715b52d84f31cb0800e0afbf9435a4
DESKTOP-T0HLH7K\os <vanchanhnguyen95@gmail.com>
2022-11-20 11:29:07 +0700

    #6: Tạo Add-in

---
+++ Study01/ArcMapAddin1/ArcMapAddin1/ButtonDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ArcMapAddin1
+{
+    public class ButtonDemo : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public ButtonDemo()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            ArcMap.Application.CurrentTool = null;
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
