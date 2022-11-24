bc6b00bcbe785c301e85cf52efc9237367c815ae
BA\chanhnv <vanchanhnguyen95@gmail.com>
2022-11-24 16:55:38 +0700

    Add src Study02

---
+++ ArcMapAddin1/Button/Button1.cs
@@ -0,0 +1,48 @@
+﻿using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Button
+{
+    public class Button1 : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public Button1()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            ArcMap.Application.CurrentTool = null;
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+
+        public void GetMxDocument()
+        {
+            //IMxDocument doc = ArcMap.Application.Document;
+            IMxDocument mxdoc = ArcMap.Document as IMxDocument;
+
+        }
+
+        public void GetMap()
+        {
+            IDocument doc = ArcMap.Application.Document;
+
+            //cast to IMxDocument interface
+            IMxDocument mxdoc = doc as IMxDocument;
+
+            IMap map = mxdoc.FocusMap;
+
+        }
+    }
+
+}
