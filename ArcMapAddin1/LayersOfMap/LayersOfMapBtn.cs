8bf7b92d501b64d08621ca8ed7a9e751bfc7d4ec
BA\chanhnv <vanchanhnguyen95@gmail.com>
2022-11-24 17:25:29 +0700

    Truy xuất Layer trong Map

---
+++ ArcMapAddin1/LayersOfMap/LayersOfMapBtn.cs
@@ -0,0 +1,69 @@
+﻿using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LayersOfMap
+{
+    public class LayersOfMapBtn : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public LayersOfMapBtn()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            ArcMap.Application.CurrentTool = null;
+
+            string data = string.Empty;
+
+            IDocument doc = ArcMap.Application.Document;
+            IMxDocument mxdoc = doc as IMxDocument;
+
+            IMap map = mxdoc.FocusMap;
+
+            // using layersCount property
+            data += "Using LayersCount Property \n";
+
+            ILayer layer;
+            for(int i= 0; i < map.LayerCount; i++)
+            {
+                layer = map.Layer[i];
+                data += " >> " + layer.Name + "\n";
+            }
+
+            // terminating layer object
+            layer = null;
+            data += string.Format("{0} Map contains {1} Layers\n", map.Name, map.LayerCount);
+            data += "-----------------------------------------\n";
+            data += "Using Layer Property \n";
+
+            IEnumLayer enumLayer = map.Layers;
+            layer = enumLayer.Next();
+            int j = 0;
+            while (layer!= null)
+            {
+                j++;
+                data += " >> " + layer.Name + "\n";
+                layer = enumLayer.Next();
+            }
+            data += string.Format("{0} Map contains {1} Layers\n", map.Name, j);
+
+            Message msgForm = new Message();
+            msgForm.lbl.Text = data;
+            msgForm.ShowDialog();
+
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
---
+++ ArcMapAddin1/LayersOfMap/Message.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace LayersOfMap
+{
+    partial class Message
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl
+            // 
+            this.lbl.Location = new System.Drawing.Point(12, 9);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(280, 400);
+            this.lbl.TabIndex = 0;
+            this.lbl.Text = "label1";
+            // 
+            // Message
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl);
+            this.Name = "Message";
+            this.Text = "Message";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label lbl;
+    }
+}
