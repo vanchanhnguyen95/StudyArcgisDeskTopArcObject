efadf33bb8c6f801b68196873f3e5da02a663162
BA\chanhnv <vanchanhnguyen95@gmail.com>
2022-11-25 18:01:09 +0700

    update message

--- ArcMapAddin1/LayersOfMap/Message.Designer.cs
+++ ArcMapAddin1/LayersOfMap/Message.Designer.cs
@@ -30,6 +30,9 @@ namespace LayersOfMap
         private void InitializeComponent()
         {
             this.lbl = new System.Windows.Forms.Label();
+            this.BtnBrowser = new System.Windows.Forms.Button();
+            this.txtBoxFilePath = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lbl
@@ -40,20 +43,51 @@ namespace LayersOfMap
             this.lbl.TabIndex = 0;
             this.lbl.Text = "label1";
             // 
+            // BtnBrowser
+            // 
+            this.BtnBrowser.Location = new System.Drawing.Point(701, 32);
+            this.BtnBrowser.Name = "BtnBrowser";
+            this.BtnBrowser.Size = new System.Drawing.Size(75, 23);
+            this.BtnBrowser.TabIndex = 1;
+            this.BtnBrowser.Text = "Browser";
+            this.BtnBrowser.UseVisualStyleBackColor = true;
+            this.BtnBrowser.Click += new System.EventHandler(this.BtnBrowser_Click);
+            // 
+            // txtBoxFilePath
+            // 
+            this.txtBoxFilePath.Location = new System.Drawing.Point(432, 35);
+            this.txtBoxFilePath.Name = "txtBoxFilePath";
+            this.txtBoxFilePath.Size = new System.Drawing.Size(263, 20);
+            this.txtBoxFilePath.TabIndex = 2;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(432, 80);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(263, 20);
+            this.txtResult.TabIndex = 3;
+            // 
             // Message
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtBoxFilePath);
+            this.Controls.Add(this.BtnBrowser);
             this.Controls.Add(this.lbl);
             this.Name = "Message";
             this.Text = "Message";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         public System.Windows.Forms.Label lbl;
+        public System.Windows.Forms.Button BtnBrowser;
+        public System.Windows.Forms.TextBox txtBoxFilePath;
+        public System.Windows.Forms.TextBox txtResult;
     }
 }
--- ArcMapAddin1/LayersOfMap/Message.cs
+++ ArcMapAddin1/LayersOfMap/Message.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +13,67 @@ namespace LayersOfMap
         {
             InitializeComponent();
         }
+
+        private async void BtnBrowser_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"D:\",
+                Title = "Browse Text Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtBoxFilePath.Text = openFileDialog1.FileName;
+                await Task.Run(() => UploadFileAsync(txtBoxFilePath.Text));
+            }
+        }
+
+        public static async Task UploadFileAsync(string path)
+        {
+            HttpClient client = new HttpClient();
+            // we need to send a request with multipart/form-data
+            var multiForm = new MultipartFormDataContent();
+
+            // add file and directly upload it
+            System.IO.FileStream fs = File.OpenRead(path);
+            multiForm.Add(new StreamContent(fs), "files", Path.GetFileName(path));
+
+            // send request to API
+            var url = "https://localhost:5001/api/v1/FileSpeedProvider/UploadFile";
+            var response = await client.PostAsync(url, multiForm);
+            if (response.IsSuccessStatusCode)
+            {
+                var contents = await response.Content.ReadAsStringAsync();
+                var responseData = JsonConvert.DeserializeObject<ResponseData>(contents);
+                MessageBox.Show(responseData.Status);
+            }
+            else
+            {
+                MessageBox.Show(response.ToString());
+            }
+
+        }
+
+        public class ResponseData
+        {
+            public string Status { get; set; }
+            public string Data { get; set; }
+            public string FilePath { get; set; }
+            public string Message { get; set; }
+
+        }
+
     }
 }
