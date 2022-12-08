02fcfdadeaeb3b4942e07de8d88cddc9e136a867
DESKTOP-T0HLH7K\os <vanchanhnguyen95@gmail.com>
2022-12-08 23:07:01 +0700

    #32: Add-in Report Schema (Phần 1)

---
+++ Bai32AddInArcMap/Bai32AddInArcMap/SimpleSchemaRepoter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geodatabase;
+
+
+namespace Bai32AddInArcMap
+{
+    public class SimpleSchemaRepoter : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public SimpleSchemaRepoter()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            ArcMap.Application.CurrentTool = null;
+
+            IMxDocument mxDoc = ArcMap.Application.Document as IMxDocument;
+
+            if(mxDoc.SelectedItem is IFeatureLayer2 || mxDoc.SelectedItem is ITable)
+            {
+                ITable selectionTbl = mxDoc.SelectedItem as ITable;
+                //reportSchema(selectionTbl);
+            }    
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
---
+++ Bai32AddInArcMap/Bai32AddInArcMap/fldClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESRI.ArcGIS.Geodatabase;
+
+namespace Bai32AddInArcMap
+{
+    class fldClass
+    {
+        public int No { get; set; }
+        public string AliasName { get; set; }
+        public string Name { get; set; }
+        public esriFieldType FieldType { get; set; }
+        public int Length { get; set; }
+        public bool IsRequired { get; set; }
+        public string IsNullable { get; set; }
+
+        public fldClass(int No)
+        {
+            this.No = No;
+        }
+
+    }
+}
